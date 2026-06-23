a8678cb02c6c1615a93cd81da583db73c8caad02
hit-robisht <rohitbisht10d@gmail.com>
2026-06-23 15:46:50 +0530

    commit

--- Day 1/table.py
+++ Day 1/table.py
@@ -1,15 +1,5 @@
-
+i=1
 n=int(input('enter the value of n:'))
-if n<0:
-   print("factorial does not exist")
-elif n==0:
-   print("the factorial of 0 is 1")
-else:
-   i=1
-   factorial=1
-   while i<=n:
-        factorial=factorial*i
-        if i==n:
-         print(factorial)
-        i+=1
-    
+while i<=10:
+    print(n*i)
+    i+=1
